355693db45b30f87e3113901ebd90ad203eb23a6
Romulo Cunha <rcunha8@its.jnj.com>
2022-10-11 15:20:22 +0100

    PLNT-410 - fix menu

--- docroot/modules/custom/planet_sitestudio_custom_elements/js/planetHeader.js
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/planetHeader.js
@@ -4,6 +4,8 @@
  */
 
 (function ($, Drupal) {
+  let menuState = 0;
+  let alreadyRun = 0;
   Drupal.behaviors.planet_sitestudio_planetHeader = {
     attach: function (context) {
       let header = document.querySelector(".header-container");
@@ -21,6 +23,36 @@
           header.classList.remove("white-bg");
         }
       });
+
+      function preventDefault(e){
+        e.preventDefault();
+      }
+
+      function disableScroll(){
+        document.body.addEventListener('touchmove', preventDefault, { passive: false });
+      }
+      function enableScroll(){
+        document.body.removeEventListener('touchmove', preventDefault);
+      }
+
+      if (alreadyRun === 0) {
+        $('.mobile-menu-button').click(function (e) {
+          e.preventDefault();
+          if (menuState === 0) {
+            // Menu is opened;
+            disableScroll();
+            menuState = 1;
+            $("body").css("overflow", "hidden");
+          } else {
+            // Menu is closed;
+            enableScroll();
+            menuState = 0;
+            $("body").css("overflow", "");
+          }
+          return false;
+        });
+        alreadyRun = 1;
+      }
     },
   };
 })(jQuery, Drupal);
