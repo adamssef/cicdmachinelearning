2bf1f469f25d8886c34c6c33e775222f9189966f
Romulo Cunha <rcunha8@its.jnj.com>
2022-10-24 11:41:32 +0100

    [PLNT-0000] GA

--- docroot/modules/custom/planet_sitestudio_custom_elements/js/ourApproachContainer.js
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/ourApproachContainer.js
@@ -13,7 +13,6 @@
           $(".coh-ce-cpt_card_feature_icon_and_text-a305ba2").each(function (index) {
             if (typeof gtag === typeof Function) {
               $('a').on('click', function (e) {
-                console.log("here");
                 gtag('event', 'Our Approach Container', {
                   'event_category': 'Content',
                   'event_label': $('h3', this).html(),
@@ -21,7 +20,6 @@
                 });
               });
             }
-            //$('a', this).attr("data-analytics", '[{"trigger":"click","eventCategory":"Content","eventAction":"Our Approach Container","eventLabel":"' + $('h3', this).html() + '","eventValue":1}]' );
           });
           alreadyRun = 1;
         }
