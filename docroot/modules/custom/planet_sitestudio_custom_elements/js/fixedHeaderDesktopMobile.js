9f753c87cfb60558105ead22ec2ce4aacf4020b8
Mariana Mendes Caspirro <CIANDT\marianam@lnb028733cps.ciandt.global>
2022-12-30 13:41:31 -0300

    [PLNT-1064] Fix navbar with white on Campaign Page
    Create script to fix white bg on nav bar for
    Campaign Page. Rename script from Hero5050
    to indicate that it fixed navbar to white only
    on desktop so it's clear that it can be
    reused when necessary.

---
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/fixedHeaderDesktopMobile.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * File planetCampaignPageTop.js.
+ */
+
+(function ($) {
+  document.addEventListener("DOMContentLoaded", (event) => {
+        $(".header-container").addClass("white-bg-fixed");
+        $(".header-container").addClass("white-bg");
+  });
+})(jQuery);
