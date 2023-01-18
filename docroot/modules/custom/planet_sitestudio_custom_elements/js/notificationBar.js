a8687f5c3bbed22964c189d08ca42c65353751b3
larissamiyaji-ciandt <larissam@ciandt.com>
2023-01-18 13:10:20 -0300

    [PLNT-1184] The mobile version is making the menu all white

---
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/notificationBar.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * File notificationBar.js.
+ */
+
+ (function ($, Drupal) {
+  Drupal.behaviors.planet_sitestudio_tabProductsPanel = {
+    attach: function () {
+      let position = jQuery(window).scrollTop();
+      if ($(window).width() < 1023) {
+        $(window).on('load', function() {
+          if(position == 0) {
+            $('header').addClass('white-bg');
+          }
+        })
+      }
+      $('.notification-bar-button').click(function(){
+        $('.notification-bar-container').addClass('closed');
+      })
+      $(document).ready(function () {
+        if($('body').find('.notification-bar-container')) {
+          if ($(window).width() < 1023) {
+          $(".notification-bar-button").click(function(){
+            $('header').removeClass('white-bg');
+          })
+        }
+          $(window).on('scroll', function(){
+            if(position == 0) {
+              $('header').addClass('white-bg');
+            }
+          })
+        }
+      })
+    }
+  };
+  })(jQuery, Drupal);
