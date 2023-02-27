aab068325f5c7bab843948beff559451855addce
Cassio Gomes do Nascimento <cgomesd2@its.jnj.com>
2023-02-27 13:50:26 -0300

    PLNT-1360: Case Studies (Slider component) scroll on logos

--- docroot/modules/custom/planet_sitestudio_custom_elements/js/csFeaturedSlider.js
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/csFeaturedSlider.js
@@ -4,24 +4,51 @@
  */
 
 (function ($, Drupal) {
+  Drupal.planet = Drupal.planet || {};
+
   Drupal.behaviors.planet_sitestudio_csFeaturedSlider = {
     attach: function () {
-      $('.cs-featured-slider-logos div:first-child').addClass('active');
-      $('.slide-logo').addClass(function(i) {
-        return 'slide-'+(i);
-      });
+      caseStudiesBlock = $('.coh-style-case-studies');
 
-      $('.coh-slider-nav-inner-top button').click(function() {
-        let index = $('.slick-active').attr('data-slick-index');
-        let slideLogoItem = 'slide-'+index;
+      caseStudiesBlock.once('case-studies-4-action').each(function () {
+        // add on click event on logo
+        Drupal.planet.transformInLink($(this));
 
-        $('.slide-logo').each(function(){
-          if($(this).hasClass(slideLogoItem)) {
-            $('.slide-logo').removeClass('active');
-            $(this).addClass('active');
-          }
+        // Add the active class to the first logo
+        $(this).find('.cs-featured-slider-logos div:first-child').addClass('active');
+
+        // Update the logos with a unique class
+        $(this).find('.slide-logo').addClass(function(i) {
+          return 'slide-'+(i);
+        });
+
+        // Update active on slide button click
+        $(this).find('.coh-slider-nav-inner-top button').click(function() {
+          const parent = $(this).parents('.coh-style-case-studies');
+          const slideIndex = parent.find('.slick-active').attr('data-slick-index');
+          const slideLogoItem = `slide-${slideIndex}`;
+  
+          parent.find('.slide-logo').each(function(){
+            if($(this).hasClass(slideLogoItem)) {
+              parent.find('.slide-logo').removeClass('active');
+              $(this).addClass('active');
+            }
+          });
         });
       });
     }
   };
+
+  // Update active on logo click
+  Drupal.planet.transformInLink = (slideDiv) => {
+    slideDiv.find('.slide-logo').on('click', function() {
+      const parent = $(this).parents('.coh-style-case-studies');
+      const slidePosition = $(this).attr('class').split('-').pop().replace(' active', '');
+
+      parent.find('.slide-logo').removeClass('active');
+      $(this).addClass('active');
+
+      parent.find(`.slick-dots li:eq(${slidePosition}) button`).click();
+    });
+  }
 })(jQuery, Drupal);
