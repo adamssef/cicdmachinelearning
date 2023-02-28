276f859e09877e1f2008ad8ce4ccec7d3ff0a72b
larissamiyaji-ciandt <larissam@ciandt.com>
2023-02-28 11:49:21 -0300

    [PLNT-1407][PLNT-1424] Changing JS to cover all scenarios

--- docroot/modules/custom/planet_sitestudio_custom_elements/js/fixedHeaderDesktop.js
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/fixedHeaderDesktop.js
@@ -5,7 +5,7 @@
 
 (function ($) {
   $(document).ready(function () {
-    if ($('.hero5050').hasClass('coh-hero-top')) {
+    if (!$('.hero5050').hasClass('coh-hero-not-top')) {
       $('.header-container').addClass('white-bg-fixed');
       $('.header-container').addClass('white-bg');
     }
