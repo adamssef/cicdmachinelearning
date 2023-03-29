1fac2affadcd400821bcdfc1892cf628fdc9827d
Florin Cotovanu <florin.cotovanu@weareplanet.com>
2023-03-29 17:40:23 +0200

    [PLNT-000-orig-lp-referral] Added cookies and send data to pardot

--- docroot/modules/custom/planet_sitestudio_custom_elements/js/planetHeader.js
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/planetHeader.js
@@ -142,6 +142,16 @@
 
         // Search for utm parameters in url.
         const urlParams = new URLSearchParams(window.location.search);
+        const shortUTMTime = 1
+        const longUTMTime = 360
+        const planetUrls = [
+          "weareplanet.com",
+          "preprod.weareplanet.com",
+          "planetpaymentdev.prod.acquia-sites.com",
+          "planet.lndo.site"
+        ]
+        const urlMaxLength = 150
+
         if (urlParams.has('utm_campaign') || urlParams.has('utm_medium') || urlParams.has('utm_source') || urlParams.has('utm_content') || urlParams.has('utm_term') || urlParams.has('gclid')){
 
           // Get the cookies.
@@ -154,47 +164,65 @@
             const utmContent = urlParams.get('utm_content')
             const utmTerm = urlParams.get('utm_term')
             const gclid = urlParams.get('gclid')
-
-            const shortUTMTime = 1
-            const longUTMTime = 60
-
-            // Save UTMs cookies
-            if (utmSource && cookies.indexOf('Drupal.visitor.utm_source') < 0) {
+           
+            /**
+             * Save recent UTMs as cookies.
+             * We want to set the cookies only if they don't exist already
+             */
+            if (utmSource && !cookieExists('Drupal.visitor.utm_source')) {
               createCookie('Drupal.visitor.utm_source', utmSource, shortUTMTime);
             }
-            if (utmMedium && cookies.indexOf('Drupal.visitor.utm_medium') < 0) {
+            if (utmMedium && !cookieExists('Drupal.visitor.utm_medium')) {
               createCookie('Drupal.visitor.utm_medium', utmMedium, shortUTMTime);
             }
-            if (utmCampaign && cookies.indexOf('Drupal.visitor.utm_campaign') < 0) {
+            if (utmCampaign && !cookieExists('Drupal.visitor.utm_campaign')) {
               createCookie('Drupal.visitor.utm_campaign', utmCampaign, shortUTMTime);
             }
-            if (utmContent && cookies.indexOf('Drupal.visitor.utm_content') < 0) {
+            if (utmContent && !cookieExists('Drupal.visitor.utm_content')) {
               createCookie('Drupal.visitor.utm_content', utmContent, shortUTMTime);
             }
-            if (utmTerm && cookies.indexOf('Drupal.visitor.utm_term') < 0) {
+            if (utmTerm && !cookieExists('Drupal.visitor.utm_term')) {
               createCookie('Drupal.visitor.utm_term', utmTerm, shortUTMTime);
             }
-            if (gclid && cookies.indexOf('Drupal.visitor.gclid') < 0) {
+            if (gclid && !cookieExists('Drupal.visitor.gclid')) {
               createCookie('Drupal.visitor.gclid', gclid, shortUTMTime);
             }
 
-            // Save original UTMs cookies
-            if (utmSource && cookies.indexOf('Drupal.visitor.orig_utm_source') < 0) {
+            /**
+             * Save original UTMs as cookies.
+             * We want to set the cookies only if they don't exist already
+             */
+            if (utmSource && !cookieExists('Drupal.visitor.orig_utm_source')) {
               createCookie('Drupal.visitor.orig_utm_source', utmSource, longUTMTime);
             }
-            if (utmMedium && cookies.indexOf('Drupal.visitor.orig_utm_medium') < 0) {
+            if (utmMedium && !cookieExists('Drupal.visitor.orig_utm_medium')) {
               createCookie('Drupal.visitor.orig_utm_medium', utmMedium, longUTMTime);
             }
-            if (utmCampaign && cookies.indexOf('Drupal.visitor.orig_utm_campaign') < 0) {
+            if (utmCampaign && !cookieExists('Drupal.visitor.orig_utm_campaign')) {
               createCookie('Drupal.visitor.orig_utm_campaign', utmCampaign, longUTMTime);
             }
-            if (utmContent && cookies.indexOf('Drupal.visitor.orig_utm_content') < 0) {
+            if (utmContent && !cookieExists('Drupal.visitor.orig_utm_content')) {
               createCookie('Drupal.visitor.orig_utm_content', utmContent, longUTMTime);
             }
-            if (utmTerm && cookies.indexOf('Drupal.visitor.orig_utm_term') < 0) {
+            if (utmTerm && !cookieExists('Drupal.visitor.orig_utm_term')) {
               createCookie('Drupal.visitor.orig_utm_term', utmTerm, longUTMTime);
             }
           }
+        }
+
+        /** 
+         * Save referrer and original_lp as cookies.
+         * referrer = the external website the user comes from
+         * original_lp = the last page the user has seen on wap before sending the lead
+         */
+        const referrer = (!containsAny(document.referrer, planetUrls)) ? document.referrer : false
+        if (referrer && !cookieExists('Drupal.visitor.referrer')) {
+          createCookie('Drupal.visitor.referrer', referrer.substring(0, urlMaxLength), shortUTMTime);
+        }
+
+        const originalLp = (containsAny(document.referrer, planetUrls)) ? document.referrer : false
+        if (originalLp && !cookieExists('Drupal.visitor.orig_lp')) {
+          createCookie('Drupal.visitor.orig_lp', originalLp.substring(0, urlMaxLength), shortUTMTime);
         }
         alreadyRun = 1;
       }
@@ -254,5 +282,18 @@
 
     document.cookie = name + "=" + value + expires + "; path=/";
   }
+
+  function cookieExists(name) {
+    return !(document.cookie.indexOf(name) === -1)
+  }
+  function containsAny(str, substrings) {
+    for (var i = 0; i != substrings.length; i++) {
+       var substring = substrings[i];
+       if (str.indexOf(substring) != - 1) {
+         return substring;
+       }
+    }
+    return null; 
+}
 
 })(jQuery, Drupal);
