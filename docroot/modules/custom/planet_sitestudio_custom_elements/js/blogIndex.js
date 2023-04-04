c9be1e165b740caf4e7624bc7ea3c2ee6f3eba02
claraciandt <99261302+claraciandt@users.noreply.github.com>
2023-04-04 09:17:31 -0300

    [PLNT-1891] Index area, scroll active state

--- docroot/modules/custom/planet_sitestudio_custom_elements/js/blogIndex.js
+++ docroot/modules/custom/planet_sitestudio_custom_elements/js/blogIndex.js
@@ -8,20 +8,43 @@
     attach: function () {
       $(document).ready(function () {
         let content = document.querySelector(".coh-layout-canvas-content");
+        let headerHeight = $(".header-container").height();
+        console.log(headerHeight)
+
+        // Get all headings on layout canvas
         let headings = content.querySelectorAll(
           ".coh-style-h700, .coh-style-h600, .coh-style-h500"
         );
-        let list = document.querySelector(".coh-index-list");
 
-        $(headings).each(function () {
+        // Get index list
+        let list = document.querySelector(".coh-index-list");
+        $(headings).each(function (i) {
+
+          let id = `coh-index-heading-${i}`;
+
+          // Add each to index list
           $(list).append(
-            `<li class="coh-list-item coh-index-item">
-              <span></span>
+            `<li id="${id}" class="coh-list-item coh-index-item">
               <p class="coh-paragraph coh-style--body-regular---tt-commons-planet">
                 ${$(this).text()}
               </p>
             </li>`
           );
+
+          let headingTop = $(this).offset().top;
+          // Check the scroll
+          $(window).scroll(function () {
+            let winScroll =
+            document.body.scrollTop || document.documentElement.scrollTop;
+            
+            // If the height scrolled is bigger than the height where is the title
+            if ((winScroll + headerHeight) > headingTop) {
+              $(`#${id}`).addClass("coh-index-item-active");
+            // If the opposite
+            } else if ((winScroll + headerHeight) < headingTop) {
+              $(`#${id}`).removeClass("coh-index-item-active");
+            }
+          });
         });
       });
     },
