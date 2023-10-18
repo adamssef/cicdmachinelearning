4ea1e9dff4e0b132ef58139fe24559f9009a4bd0
Florin Cotovanu <florin.cotovanu@weareplanet.com>
2023-10-19 00:30:04 +0200

    added load more

--- docroot/modules/custom/planet_core/src/Controller/AuthorArticles.php
+++ docroot/modules/custom/planet_core/src/Controller/AuthorArticles.php
@@ -1,14 +1,14 @@
 <?php
-namespace Drupal\planet_language_switcher\Controller;
+namespace Drupal\planet_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthorArticles extends ControllerBase
 {
-    public function loadAuthorArticles($author_id)
+    public function loadAuthorArticles($author_id, $limit, $offset)
     {
-        $author_articles = planet_language_switcher_get_author_articles($author_id);
+        $author_articles = planet_core_get_author_articles($author_id, $limit, $offset);
         return new JsonResponse($author_articles);
     }
 }
