35023f8d3cbbf0d4581f7dee240305c59b0e90ba
adamyoussef0 <adam.youssef@weareplanet.com>
2024-02-07 19:12:21 +0100

    Fix missing picture and refactor on pms-transformation landing page

--- docroot/modules/custom/planet_core/js/PmsTransformationPage.js
+++ docroot/modules/custom/planet_core/js/PmsTransformationPage.js
@@ -1,10 +1,6 @@
 (function ($, Drupal) {
   Drupal.behaviors.customScript = {
     attach: function (context, settings) {
-      function scrollToElement() {
-        document.getElementById("form-50-50").scrollIntoView({ behavior: 'smooth' });
-      }
-
       let formLabelsRequired = $('.js-form-required');
 
       formLabelsRequired.each(function () {
@@ -12,25 +8,6 @@
 
         if (label.slice(-1) === '*') {
          $(this).html(label.slice(0, -1) + '<span class="required" style="color:red">*</span>');
-        }
-      });
-
-      var windowWidth = $(window).width();
-
-      if (windowWidth < 768) {
-        $('#cloud-vision-image').attr('src', '/resources/images/Cloudvision_Mobile.svg');
-      } else {
-        $('#cloud-vision-image').attr('src', '/resource/images/Cloudvision_DesktopTablet.svg');
-      }
-
-
-      $(window).resize(function () {
-        windowWidth = $(window).width();
-
-        if (windowWidth < 768) {
-          $('#cloud-vision-image').attr('src', '/resources/images/Cloudvision_Mobile.svg');
-        } else {
-          $('#cloud-vision-image').attr('src', '/resources/images/Cloudvision_DesktopTablet.svg');
         }
       });
     }
