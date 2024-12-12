5bb2a938aff6ddb4a5ceb1e696892337fb125a70
Adam Youssef <adam.youssef@weareplanet.com>
2024-12-12 10:37:28 +0100

    Disable cache from footer block

--- docroot/modules/custom/planet_footer/src/Plugin/Block/PlanetFooterBlock.php
+++ docroot/modules/custom/planet_footer/src/Plugin/Block/PlanetFooterBlock.php
@@ -68,6 +68,7 @@ class PlanetFooterBlock extends BlockBase implements ContainerFactoryPluginInter
       'footer_menu' => $this->blockService->prepareLinksForFooter(),
       '#current_language_prefix' => $this->blockService->languageManager->getCurrentLanguage()->getId(),
       'lang_switcher_data' => $this->blockService->prepareDataForLanguageSwitcher(),
+      '#cache' => ['max-age' => 0]
     ];
 
     return $block_array;
