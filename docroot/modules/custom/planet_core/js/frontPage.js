d188c82886f418a43d6d21c8339984b5fb7a5c09
Florin Cotovanu <florin.cotovanu@weareplanet.com>
2025-05-14 10:13:59 +0200

    fixed hero animation

---
+++ docroot/modules/custom/planet_core/js/frontPage.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function () {
+
+  if (jQuery('.orb-1').length > 0) {
+    jQuery('.orb-1').addClass('orb-1-animation').fadeIn(1000);
+
+  }
+
+  if (jQuery('.orb-2').length > 0) {
+    jQuery('.orb-2').addClass('orb-2-animation').fadeIn(1000);
+  }
+
+
+
+});
